2b856c291e1bf41c3617275da0d1972decb6ee1d
Tsira otarashvili <120280898+tsira19@users.noreply.github.com>
2023-10-27 01:01:51 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var numbers=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+document.write(numbers+ "<br>");
+let i = 0; 
+let x = 0;
+while(x<numbers.length){
+    i+=numbers[x];
+    
+    x++;
+}
+document.write(i);
+
+
